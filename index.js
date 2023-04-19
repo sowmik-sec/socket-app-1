dbdc0cd54ca299e23b83fc1aef30dd5b63def79e
sowmik-sec <sowmik.sec@gmail.com>
2023-04-19 12:21:57 +0600

    broadcast

--- index.js
+++ index.js
@@ -7,31 +7,47 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-io.on("connection", (socket) => {
-  console.log("New User Connected");
+const buyNsp = io.of("/buy");
+const sellNsp = io.of("/sell");
 
-  socket.on("message", (msg) => {
-    console.log(msg);
-  });
-
-  //   setInterval(() => {
-  //     const d = new Date();
-  //     const t = d.getTime();
-  //     socket.emit("myEvent", t);
-  //   }, 10);
-  //   setInterval(() => {
-  //     const d = new Date();
-  //     const t = d.getTime();
-  //     socket.send(t);
-  //   }, 1000);
-
-  //   setTimeout(() => {
-  //     socket.send("Learning socket.io (server-->client)");
-  //   }, 5000);
-  //   socket.on("disconnect", () => {
-  //     console.log("User Disconnected");
-  //   });
+buyNsp.on("connection", (socket) => {
+  buyNsp.emit("myEvent", "hello buy");
 });
+
+sellNsp.on("connection", (socket) => {
+  sellNsp.emit("myEvent", "hello sell");
+});
+
+// io.on("connection", (socket) => {
+//   console.log("New User Connected");
+
+//   io.sockets.emit("myBroadcast", "Hello Everyone!");
+
+//   socket.on("myEvent", (msg) => {
+//     console.log(msg);
+//   });
+//   socket.on('message', (msg)=> {
+//     console.log(msg);
+//   })
+
+//   setInterval(() => {
+//     const d = new Date();
+//     const t = d.getTime();
+//     socket.emit("myEvent", t);
+//   }, 10);
+//   setInterval(() => {
+//     const d = new Date();
+//     const t = d.getTime();
+//     socket.send(t);
+//   }, 1000);
+
+//   setTimeout(() => {
+//     socket.send("Learning socket.io (server-->client)");
+//   }, 5000);
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected");
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
