8b0efa2e7571d1393c0d135ef0768358b5531f6e
sowmik-sec <sowmik.sec@gmail.com>
2023-04-19 11:02:52 +0600

    discover connection disconnection and ws protocol

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const http = require("http");
 const port = process.env.PORT || 5000;
 const expressServer = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+
+io.on("connection", (socket) => {
+  console.log("New User Connected");
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
